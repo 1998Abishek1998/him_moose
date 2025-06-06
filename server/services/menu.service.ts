15fd55d7e2d9320fd84ca66d1aa780e5993471c4
1998Abishek1998 <uzamakiu@gmail.com>
2025-06-06 07:58:10 +0545

    menu handler added

---
+++ server/services/menu.service.ts
@@ -0,0 +1,21 @@
+import { Context } from "hono";
+
+const createMenu = async(c: Context) => {
+    
+}
+
+const getMenuAdmin = async(c: Context) => {
+
+}
+
+const getMenuRequest = async(c: Context) => {
+
+}
+
+const updateMenu = async(c: Context) => {
+
+}
+
+const deleteMenu = async(c: Context) => {
+
+}
